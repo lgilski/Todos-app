0ea047b631c5f9721e0285beae88836e45977ef9
COUGAR <COUGAR@gmail.com>
2023-03-07 20:38:56 +0100

    Better styles

--- src/components/UI/Subtitle.js
+++ src/components/UI/Subtitle.js
@@ -5,7 +5,7 @@ import classes from './Subtitle.module.css';
 const Subtitle = function () {
   return (
     <div className={classes.wrapper}>
-      <h2 className={classes.subtitle}>Your Todos</h2>
+      <h2 className={classes.subtitle}>Your To-dos</h2>
       <p>and stuff UwU</p>
     </div>
   );
