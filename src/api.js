9b6eb4074253d7981989c13759c1a5592a224382
COUGAR <COUGAR@gmail.com>
2023-06-18 20:49:19 +0200

    test

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@
  */
 export async function fetchWeather({ city }) {
   const response = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_KEY}&q=${city}&aqi=no`
+    `https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_KEY}&q=${city}&aqi=no`
   );
 
   const data = await response.json();
