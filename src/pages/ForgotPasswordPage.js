c638d76045de845d0f3578038cca7d77651bc95a
COUGAR <COUGAR@gmail.com>
2023-07-05 15:56:36 +0200

    One more alert message

--- src/pages/ForgotPasswordPage.js
+++ src/pages/ForgotPasswordPage.js
@@ -2,6 +2,7 @@ import { redirect } from 'react-router-dom';
 import ForgotPasswordForm from '../components/UI/ForgotPasswordForm/ForgotPasswordForm';
 import { auth } from '../config/firebase';
 import { sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 // type,
 //     name,
@@ -24,6 +25,17 @@ export async function action({ request }) {
     const email = data.get('email');
 
     sendPasswordResetEmail(auth, email);
+
+    toast.success('Reset password email has been sent.', {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+    });
 
     return redirect('/');
   } catch (err) {}
