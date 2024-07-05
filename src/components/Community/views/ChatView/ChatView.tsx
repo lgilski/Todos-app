4fdb35ff40d2049c7e75333e7d22421991106e1c
COUGAR <COUGAR@gmail.com>
2024-07-05 11:06:20 +0200

    test

--- src/components/Community/views/ChatView/ChatView.tsx
+++ src/components/Community/views/ChatView/ChatView.tsx
@@ -95,19 +95,19 @@ function ChatView({
 
     let timer = null;
 
-    if (
-      // When someone you are chating with sends message, stop showing that he/she is typing
+    // if (
+    //   // When someone you are chating with sends message, stop showing that he/she is typing
 
-      myMessages[myMessages.length - 1]?.sender ===
-        currentFriend?.uid &&
-      lastWritingTimestamp &&
-      myMessages[myMessages.length - 1]?.date > lastWritingTimestamp
-    ) {
-      setIsTyping(false);
-    }
+    //   myMessages[myMessages.length - 1]?.sender ===
+    //     currentFriend?.uid &&
+    //   lastWritingTimestamp &&
+    //   myMessages[myMessages.length - 1]?.date > lastWritingTimestamp
+    // ) {
+    //   setIsTyping(false);
+    // }
 
     // If last typing was at max 5 seconds ago, display that he/she is typing and after 5 seconds stop displaying
-    else if (
+    if (
       lastWritingTimestamp &&
       (lastWritingTimestamp - Date.now()) / 1000 > -5
     ) {
